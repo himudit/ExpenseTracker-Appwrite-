3eaff007b8bd884b4323f7fda09fe8c9f0e01d71
himudit <gargmudit662@gmail.com>
2024-09-06 18:29:53 +0530

    first auth entry

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+// import Search from './components/Search'
+// import RecipeApp from './components/RecipeApp'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
+// import Favourite from './components/Favourite'
+import Layout from './Layout.jsx'
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Profile from './components/Profile.jsx'
+// import RecipeAi from './components/RecipeAi.jsx'
+import Home from './components/Home.jsx'
+// import CookingAnimation from "./components/Preloader/Preloader.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout />}>
+        <Route path='/' element={<Login />} />
+        <Route path='signup' element={<Signup />} />
+        <Route path='home' element={<Home />} />
+        <Route path='profile' element={<Profile />} />
+        {/* <Route path='search' element={<Search />} /> */}
+        {/* <Route path='favourite' element={<Favourite />} /> */}
+        {/* <Route path='recipeAI' element={<RecipeAi />} /> */}
+        {/* <Route path='preloader' element={<CookingAnimation />} /> */}
+      </Route>
+    )
+  );
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
     </>
   )
 }
