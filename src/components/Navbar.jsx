aa89c76f6e6d656cd86e6239edc26a2d43e7c740
himudit <gargmudit662@gmail.com>
2024-12-02 21:20:50 +0530

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -80,7 +80,7 @@ function Navbar() {
           {
             userDetails ? <div>
               <button
-                className="rounded-full bg-red-200 px-3 py-2 text-sm font-semibold text-black shadow-sm hover:bg-lime-green/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                className="rounded-full bg-red-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-lime-green/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                 onClick={handleLogout}
               >
                 <FontAwesomeIcon icon={faRightFromBracket} />
